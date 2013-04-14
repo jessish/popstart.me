b3ba7f4c8eb8d0a0e9d7625ffbe86b04e82b6c06
Lyn Nagara <lyn.nagara@gmail.com>
2013-04-14 13:51:46 +1000

    remove js references

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @registrations = Registration.all
+    @registration = Registration.new
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @registrations }
+    end
+
+
   end
 end
