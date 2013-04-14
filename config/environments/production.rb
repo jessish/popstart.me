bb3eb17cc9f8e54c5965a04021ae60ca3e66e7c2
colin <colin@TheSharingChariot.(none)>
2013-04-14 16:02:09 +1000

    fixing styles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,6 @@ SampleApp::Application.configure do
   }
 
   config.assets.precompile += ["registrations.css", "bootstrap.min.css","bootstrap-responsive.min.css","glyphicons-halflings.png","glyphicons-halflings-white.png","bootstrap.min.js"]
-  config.assets.precompile += ["normalize.css", "foundation.css", "prettyPhoto.css", "mainStyles.css", "camera.css", "simplemodal.css", "home.css", "popstart_form.css", "foundation.min.js", "foundation.orbit.js", "jquery.arctext.js", "jquery.easing.1.3.js", "jquery.mobile.customized.min.js", "jquery.prettyPhoto.js", "jquery.tweet.js", "jquery.simplemodal.js", "zepto.min.js", "custom.modernizr.js", "filterable.pack.js", "novakScript.js","camera.js"]
+  config.assets.precompile += ["normalize.css", "foundation.css", "prettyPhoto.css", "mainStyles.css", "camera.css", "simplemodal.css", "home.css", "popstart_form.css", "foundation.min.js", "foundation.orbit.js", "jquery.arctext.js", "jquery.easing.1.3.js", "jquery.mobile.customized.min.js", "jquery.prettyPhoto.js", "jquery.tweet.js", "jquery.simplemodal.js", "zepto.min.js", "zepto.js", "custom.modernizr.js", "filterable.pack.js", "novakScript.js","camera.js"]
 
 end
